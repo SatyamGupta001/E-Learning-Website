cd807460f74b8b591a732669fda591a1ee3d5d8e
SatyamGupta001 <117983191+SatyamGupta001@users.noreply.github.com>
2025-03-06 21:22:41 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import AllRoute from "./routes/AllRoute";
+import Navbar from "./components/UserComponents/UserNavbar";
+
+
+function App() {
+  const userStore = useSelector((store)=>store.UserReducer);
+  return (
+    <div className="App">
+    {userStore?.role === "admin" || userStore?.role==='teacher' ? <Navbar/> : null}
+      <AllRoute />
+    </div>
+  );
+}
+
+export default App;
